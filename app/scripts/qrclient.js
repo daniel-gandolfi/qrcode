9f5713922067378ef914f502314d7f06711374f7
Daniel Gandolfi <gandan97@hotmail.it>
2019-04-22 18:52:30 +0200

    switch to web worker and remove no longer useful script import

--- app/scripts/qrclient.js
+++ app/scripts/qrclient.js
@@ -1,13 +1,21 @@
 var QRClient = function() {
-
+  var qrDecoderWorker;
   var currentCallback;
   
   this.decode = function(imageData, callback) {
     try {
-      var width = imageData.width;
-      var height = imageData.height;
-      var result = qrcode.decode(width, height, imageData);
-      callback(result);
+      if (window.Worker) {
+        qrDecoderWorker = qrDecoderWorker || new Worker("scripts/jsqrcode/qrworker.js");
+        qrDecoderWorker.postMessage(imageData);
+        qrDecoderWorker.onmessage = function(message){
+          callback(message.data);
+        };
+      } else {
+        // var width = imageData.width;
+        // var height = imageData.height;
+        // var result = qrcode.decode(width, height, imageData);
+        // callback(result);
+      }
     } 
     catch(e) {
       // consume the error.
